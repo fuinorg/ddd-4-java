dbac3364e23af790c4069fee19e4dbb6a9cfcce5
Michael Schnell <michael@fuin.org>
2016-07-16 15:20:50 +0200

    Moved test class

--- src/test/java/org/fuin/ddd4j/ddd/GeneralTests.java
+++ src/test/java/org/fuin/ddd4j/ddd/GeneralTests.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library. If not, see http://www.gnu.org/licenses/.
  */
-package org.fuin.ddd4j.common;
+package org.fuin.ddd4j.ddd;
 
 import java.io.File;
 

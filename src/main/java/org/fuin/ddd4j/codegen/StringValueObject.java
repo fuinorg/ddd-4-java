6cb3c14809a6c9f4ca265ad164905eba3c39a49a
Michael Schnell <michael@fuin.org>
2020-05-11 19:57:59 +0200

    Added new generator annotation

---
+++ src/main/java/org/fuin/ddd4j/codegen/StringValueObject.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (C) 2015 Michael Schnell. All rights reserved. 
+ * http://www.fuin.org/
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see http://www.gnu.org/licenses/.
+ */
+package org.fuin.ddd4j.codegen;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Value object based on a single string.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface StringValueObject {
+
+    /**
+     * Unique name of the type to generate.
+     * 
+     * @return Simple name of the target class.
+     */
+    String name();
+
+    /**
+     * Generate a JPA converter.
+     * 
+     * @return TRUE generates a converter.
+     */
+    boolean jpa() default false;
+
+    /**
+     * Generate a JAXB adapter.
+     * 
+     * @return TRUE generates an adapter.
+     */
+    boolean jaxb() default false;
+
+    /**
+     * Generate a JSONB adapter.
+     * 
+     * @return TRUE generates an adapter.
+     */
+    boolean jsonb() default false;
+
+    /**
+     * Generate a Eclipse Microprofile OpenAPI annotations.
+     * 
+     * @return TRUE generates annotations.
+     */
+    boolean openapi() default false;
+
+    /**
+     * Description of the type.
+     * 
+     * @return Human readable business driven description of what this type represents.
+     */
+    String description() default "";
+
+    /**
+     * Sets the maximum length of the type (&gt;=0).
+     * 
+     * @return Maximum length.
+     */
+    int maxLength() default Integer.MAX_VALUE;
+
+    /**
+     * Sets the minimum length of the type (&gt;=0).
+     * 
+     * @return Minimum length.
+     */
+    int minLength() default 0;
+
+    /**
+     * A pattern that describes the content of the type.
+     * 
+     * @return Regular expression.
+     */
+    String pattern() default "";
+
+    /**
+     * Example value of the type.
+     * 
+     * @return Example value.
+     */
+    String example() default "";
+
+    /**
+     * Serial version UID to use.
+     * 
+     * @return Version used for Java serialization.
+     */
+    long serialVersionUID() default 1L;
+    
+}
